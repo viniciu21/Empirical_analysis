26574647f42573d42aecf606375f7528d8b8be98
Diego Filgueiras <diegofilbal@ufrn.edu.br>
2020-09-16 11:51:46 -0300

    amend searchAlgorithm.cpp comments

--- src/searchAlgorithm.cpp
+++ src/searchAlgorithm.cpp
@@ -1,65 +1,38 @@
 #include "../include/search.h"
 
-int *bsearch(int *first, int *last, short target)
-{
+int *bsearch(int *first, int *last, short target){
 
-	// Holds the array's elements' number
-	int count = last - first;
+	int count = last - first;	// Holds the array's elements' number
 
-	// Keeps running across the array while the array is not done
-	while (count != 0)
-	{
+	while (count != 0){	// Keeps running across the array while the array is not done
 
-		// Evaluates the step value
-		int step{count / 2};
-		// Evaluates the middle element pointer
-		int *mid = first + step;
+		int step{count / 2};	// Evaluates the step value
+		int *mid = first + step;	// Evaluates the middle element pointer
+		
+		if (target == (*mid))	// Found the target
+			return mid; // The function is done and it returns a pointer to the element
 
-		// Found the target
-		if (target == (*mid))
+		else{	// The target wasn't found
+			if (target < (*mid))	// The target is on the left hand side of the array
+				count = step;	// Resizes the current array turning it into the left half sub array
 
-			// The function is done and it returns a pointer to the element
-			return mid;
-
-		// The target wasn't found
-		else
-		{
-
-			// The target is on the left hand side of the array
-			if (target < (*mid))
-
-				// Resizes the current array turning it into the left half sub array
-				count = step;
-
-			// The target is on the right hand side of the array
-			else
-			{
-
-				// Resizes the current array turning it into the right half sub array
-				first = mid + 1;
-				count = count - step - 1;
+			else{	// The target is on the right hand side of the array
+				first = mid + 1;	// Resizes the current array turning it into the right half sub array
+				count = count - step - 1;	// Updates the number of elements of the array
 			}
 		}
 	}
-
 	return last; // Failed
 };
 
-int *lsearch(int *first, int *last, short target)
-{
+int *lsearch(int *first, int *last, short target){
 
-	// Traver the array
-	while (first != last)
-	{
-
-		// Found the target
-		if (target == (*first))
-			// Returns the element pointer
-			return first;
+	while (first != last){	// Travel the array
+		if (target == (*first))	// Found the target
+			return first;	// Returns the element pointer
 
 		first++;
 	}
 
-	// Failed
-	return last;
+	return last;	// Failed
 };
