63ffe64201b36603071449352bdb666ba2529446
viniciu21 <viniciusoliveiras12@hotmail.com.br>
2020-09-16 10:41:06 -0300

    Comments

--- src/main.cpp
+++ src/main.cpp
@@ -9,27 +9,27 @@ using namespace std;
 int main()
 {
 
-	ofstream linearf, binaryf;
+	ofstream linearf, binaryf; //Arquivos de saida
 	linearf.open("data/linear.txt");
 	binaryf.open("data/binary.txt");
 
-	short target{1};
+	short target{1}; //O que nosso algoritmo vai buscar
 	long double bin_values[100], lin_values[100];
 
 	for (int indexI{50}; indexI <= 100000; indexI += 50)
 	{
 
-		int *samples = new int[indexI];
-		fill_n(samples, indexI, 0);
+		int *samples = new int[indexI]; //Inicialização do array
+		fill_n(samples, indexI, 0);		//Enchendo o array com zeros
 
-		int *begin{samples};
+		int *begin{samples}; //Ponteiros para o inicio e o fim do array
 		int *end{(samples + indexI) - 1};
 
-		long double lin_sum{0}, bin_sum{0}, lin_average, bin_average;
+		long double lin_sum{0}, bin_sum{0}, lin_average, bin_average; //Somatório e medias
 
 		for (int indexJ{0}; indexJ < 100; indexJ++)
 		{
-
+			//Tempo de execução da busca linear
 			auto start_time = chrono::steady_clock::now();
 
 			lsearch(begin, end, target);
@@ -39,6 +39,7 @@ int main()
 
 			lin_values[indexJ] = chrono::duration<long double, milli>(difference).count();
 
+			//Tempo de execução da busca binária
 			start_time = chrono::steady_clock::now();
 
 			bsearch(begin, end, target);
@@ -48,7 +49,7 @@ int main()
 
 			bin_values[indexJ] = chrono::duration<long double, milli>(difference).count();
 		}
-
+		// Média dos tempos
 		for (int indexJ{0}; indexJ < 100; indexJ++)
 		{
 			lin_sum += lin_values[indexJ];
@@ -58,10 +59,11 @@ int main()
 		lin_average = lin_sum / 100;
 		bin_average = bin_sum / 100;
 
+		//Imprimindo as médias nos arquivos de saída
 		linearf << indexI << " " << lin_average << endl;
 		binaryf << indexI << " " << bin_average << endl;
 	}
-
+	//Fechando as instancias arquivos de saida
 	linearf.close();
 	binaryf.close();
 
